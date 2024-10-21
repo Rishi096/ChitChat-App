19910e2bc619a426110ef24aa70ac4ebf83fdf8f
Rishi096 <rishirajtiwari200002@gmail.com>
2024-10-21 15:10:29 +0530

    Updation in Socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ( {children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("https://chitchat-app-prod.onrender.comg",{
+            const socket = io("https://chitchat-app-prod.onrender.com",{
                 query: {
                     userId : authUser._id,
                 }
